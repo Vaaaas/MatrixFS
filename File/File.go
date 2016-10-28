799f9d6a2143dcdad4b6cd158741ea85352dd727
Vaaaas <yanghp0907@gmail.com>
2016-10-28 13:32:54 +0800

    File handle start
    1.create SysConfig pkg & File pkg
    2.Init SysConfig(FaultNum, RowNum, SliceNum, DataNum, RddtNum)
    3.Init Temp Folders
    4.Handle file
        (1)Copy file to temp folder
        (2)Split file to data blocks(if FillSize == SliceSize, can’t init
    the last block)
        (3)Init rddt blocks(perhaps trouble)
        (4)Get file from server

---
+++ File/File.go
@@ -0,0 +1,305 @@
+package File
+
+import (
+	"strings"
+	"os"
+	"Vaaaas/MatrixFS/SysConfig"
+	"github.com/golang/glog"
+	"errors"
+	"io"
+	"strconv"
+	"math"
+)
+
+//使用时需要通过File(包名).File(结构体名)来访问
+type File struct {
+	FileFullName string
+	Size         int64
+	FillLast     bool
+	FillSize     int
+	SliceSize    int64
+}
+
+func (file *File) Init(source string) error {
+	fileInfo, err := os.Stat(source)
+	if err != nil {
+		glog.Error(err)
+		panic(err)
+	}
+	if fileInfo.IsDir() {
+		glog.Error("初始化失败：该路径指向的是文件夹")
+		return errors.New("初始化失败：该路径指向的是文件夹")
+	} else {
+		file.FileFullName = fileInfo.Name()
+		file.Size = fileInfo.Size()
+		if ((file.Size % (int64)(SysConfig.SysConfig.SliceNum)) != 0) {
+			file.FillLast = true
+			file.SliceSize = file.Size / (int64)(SysConfig.SysConfig.SliceNum) + 1
+			file.FillSize = (int)(file.SliceSize * (int64)(SysConfig.SysConfig.SliceNum) - file.Size)
+		} else {
+			file.FillLast = false
+			file.SliceSize = file.Size / (int64)(SysConfig.SysConfig.SliceNum)
+			file.FillSize = 0
+		}
+		glog.Infof("%+v ", file)
+		//将文件复制到temp目录
+		file.cpyFile(source)
+		return nil
+	}
+}
+
+func (file File) cpyFile(source string) (error) {
+	in, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create("./temp/" + file.FileFullName)
+	if err != nil {
+		glog.Error(err)
+		panic(err)
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	if _, err = io.Copy(out, in); err != nil {
+		glog.Error(err)
+		panic(err)
+	}
+	err = out.Sync()
+	return nil
+}
+
+func (file File) SliceFileName() (string, string) {
+	if file.FileFullName != "" {
+		slices := strings.Split(file.FileFullName, ".")
+		if len(slices) > 1 {
+			n := len(".") * (len(slices) - 1)
+			for i := 0; i < len(slices) - 1; i++ {
+				n += len(slices[i])
+			}
+			name := make([]byte, n - 1)
+			bp := copy(name, slices[0])
+			for _, s := range slices[1:len(slices) - 1] {
+				bp += copy(name[bp:], ".")
+				bp += copy(name[bp:], s)
+			}
+			exten := slices[len(slices) - 1]
+			return string(name), exten
+		} else {
+			glog.Error("分割文件名失败：该文件不含扩展名")
+			return "", ""
+		}
+	} else {
+		glog.Error("分割文件名失败：未定义文件名")
+		return "", ""
+	}
+}
+
+//todo : 代码有重复，待优化
+func (file File) InitDataFiles() error {
+	source := "./temp/" + file.FileFullName
+
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		glog.Error("分割时打开源文件失败")
+		panic(err)
+	}
+	defer func() {
+		if err := sourceFile.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	buffer := make([]byte, file.SliceSize)
+	for i := 0; i < SysConfig.SysConfig.SliceNum; i++ {
+		n, err := sourceFile.Read(buffer)
+		if err != nil && err != io.EOF {
+			glog.Error("buffer读取文件失败 " + strconv.Itoa(i))
+			panic(err)
+		}
+		if n == 0 {
+			break;
+		}
+
+		dataPosition := i / SysConfig.SysConfig.RowNum
+		rowPosition := i % SysConfig.SysConfig.RowNum
+		outFile, err := os.Create("./temp/Data." + strconv.Itoa(dataPosition) + "/" + file.FileFullName + "." + strconv.Itoa(dataPosition) + strconv.Itoa(rowPosition))
+		if err != nil {
+			glog.Error("新建数据分块文件失败 " + "./temp/Data." + strconv.Itoa(dataPosition) + "/" + file.FileFullName + "." + strconv.Itoa(dataPosition) + strconv.Itoa(rowPosition))
+			panic(err)
+		}
+		defer func() {
+			if err := outFile.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		if (file.FillLast&&i == SysConfig.SysConfig.SliceNum - 1) {
+			if (int64)(file.FillSize) != file.SliceSize {
+				start := file.SliceSize - (int64)(file.FillSize) - 1
+				for j := 0; j < file.FillSize; j++ {
+					buffer[start + (int64)(j)] = (byte)(0)
+				}
+
+				if _, err := outFile.Write(buffer[:file.SliceSize]); err != nil {
+					glog.Error("写入数据分块失败 " + strconv.Itoa(i))
+					panic(err)
+				}
+			} else {
+
+				for j := 0; j < file.FillSize; j++ {
+					buffer[j] = (byte)(0)
+				}
+
+				if _, err := outFile.Write(buffer[:file.FillSize]); err != nil {
+					glog.Error("写入数据分块失败 " + strconv.Itoa(i))
+					panic(err)
+				}
+			}
+
+		} else {
+			if _, err := outFile.Write(buffer[:n]); err != nil {
+				glog.Error("写入数据分块失败 " + strconv.Itoa(i))
+				panic(err)
+			}
+		}
+	}
+	return nil
+}
+
+func (file File) GetFile(targetFolder string) error {
+	target, err := os.Create(targetFolder + file.FileFullName)
+	if err != nil {
+		glog.Error("无法新建目标文件")
+		panic(err)
+	}
+	defer func() {
+		if err := target.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	buffer := make([]byte, file.SliceSize)
+	for i := 0; i < SysConfig.SysConfig.SliceNum; i++ {
+		dataPosition := i / SysConfig.SysConfig.RowNum
+		rowPosition := i % SysConfig.SysConfig.RowNum
+
+		dataFile, err := os.Open("./temp/Data." + strconv.Itoa(dataPosition) + "/" + file.FileFullName + "." + strconv.Itoa(dataPosition) + strconv.Itoa(rowPosition))
+		if err != nil {
+			glog.Error("读取数据分块文件失败 " + "./temp/Data." + strconv.Itoa(dataPosition) + "/" + file.FileFullName + "." + strconv.Itoa(dataPosition) + strconv.Itoa(rowPosition))
+			panic(err)
+		}
+		defer func() {
+			if err := dataFile.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		n, err := dataFile.Read(buffer)
+		if err != nil && err != io.EOF {
+			glog.Error("buffer读取文件失败 " + strconv.Itoa(i))
+			panic(err)
+		}
+		if n == 0 {
+			break;
+		}
+
+		if (file.FillLast&&i == SysConfig.SysConfig.SliceNum - 1) {
+			if (int64)(file.FillSize) != file.SliceSize {
+				if _, err := target.Write(buffer[:(n - file.FillSize)]); err != nil {
+					glog.Error("写入数据分块失败 " + strconv.Itoa(i))
+					panic(err)
+				}
+			}
+		} else {
+			if _, err := target.Write(buffer[:n]); err != nil {
+				glog.Error("写入数据分块失败 " + strconv.Itoa(i))
+				panic(err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (file File)InitRddtFiles() error {
+	rddtFolderCounter := 0
+	rddtRowCounter := 0
+	for faultCount := 0; faultCount < SysConfig.SysConfig.FaultNum; faultCount++ {
+		k := (int)((faultCount + 2) / 2 * (int)(math.Pow(-1, (float64)(faultCount + 2))))
+		for i := 0; i < SysConfig.SysConfig.DataNum; i++ {
+			if (rddtRowCounter % SysConfig.SysConfig.RowNum == 0&&(i != 0 || faultCount != 0)) {
+				rddtRowCounter = 1;
+				rddtFolderCounter++;
+			} else {
+				rddtRowCounter++;
+			}
+			file.initOneRddtFile(i, k, rddtFolderCounter)
+		}
+	}
+	return nil
+}
+
+func (file File)initOneRddtFile(startFolderNum, k, rddtNum int) error {
+	rddtFilePath, err := os.Create("./temp/Rddt." + strconv.Itoa(rddtNum) + "/" + file.FileFullName + "." + strconv.Itoa(k) + strconv.Itoa(startFolderNum))
+	if err != nil {
+		glog.Error("新建冗余分块文件失败 " + "./temp/Rddt." + strconv.Itoa(rddtNum) + "/" + file.FileFullName + "." + strconv.Itoa(k) + strconv.Itoa(startFolderNum))
+		panic(err)
+	}
+	defer func() {
+		if err := rddtFilePath.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	buffer := make([]byte, file.SliceSize)
+
+	for i := 0; i < SysConfig.SysConfig.RowNum; i++ {
+		folderPosi := startFolderNum + k * i
+		if folderPosi >= SysConfig.SysConfig.DataNum {
+			folderPosi = folderPosi - SysConfig.SysConfig.DataNum
+		} else if folderPosi < 0 {
+			folderPosi = SysConfig.SysConfig.DataNum + folderPosi
+		}
+
+		sourceFile, err := os.Open("./temp/Data." + strconv.Itoa(folderPosi) + "/" + file.FileFullName + "." + strconv.Itoa(folderPosi) + strconv.Itoa(i))
+		if err != nil {
+			glog.Error("生成冗余文件 " + "./temp/Rddt." + strconv.Itoa(rddtNum) + "/" + file.FileFullName + "." + strconv.Itoa(k) + strconv.Itoa(startFolderNum) + " 时打开源文件失败 " + "./temp/Data." + strconv.Itoa(folderPosi) + "/" + file.FileFullName + "." + strconv.Itoa(folderPosi) + strconv.Itoa(i))
+			panic(err)
+		}
+		defer func() {
+			if err := sourceFile.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		if (i == 0) {
+			_, err = sourceFile.Read(buffer)
+			if err != nil && err != io.EOF {
+				glog.Error("buffer读取文件失败 " + strconv.Itoa(i))
+				panic(err)
+			}
+		} else {
+			tempBytes := make([]byte, file.SliceSize)
+			_, err = sourceFile.Read(tempBytes)
+			if err != nil && err != io.EOF {
+				glog.Error("tempBuffer读取文件失败" + strconv.Itoa(i))
+				panic(err)
+			}
+			for byteCounter := 0; byteCounter < len(buffer); byteCounter++ {
+				buffer[byteCounter] = buffer[byteCounter] ^ tempBytes[byteCounter]
+				glog.Info(buffer[byteCounter] + buffer[byteCounter] + tempBytes[byteCounter])
+
+			}
+		}
+		if i == SysConfig.SysConfig.RowNum - 1 {
+			if _, err := rddtFilePath.Write(buffer[:file.SliceSize]); err != nil {
+				glog.Error("写入冗余分块文件失败 " + "./temp/Rddt." + strconv.Itoa(rddtNum) + "/" + file.FileFullName + "." + strconv.Itoa(k) + strconv.Itoa(startFolderNum))
+				panic(err)
+			}
+		}
+	}
+	return nil
+}
